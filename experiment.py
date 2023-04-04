f51937185fc80dad8e67c85f569fd47b2aa0f325
EduardoRomero83 <earg8395@hotmail.com>
2023-04-04 16:43:22 -0400

    .

--- experiment.py
+++ experiment.py
@@ -8,13 +8,13 @@ from trainOgForest import manipDataR
 Hyperparameters for model. If multiple, then all combinations
 of parameters are executed.
 """
-numTrees = [str(x) for x in [10]]
-depth = [str(x) for x in [8]]
-mpc = [str(x) for x in [15]]
-maxUnk = [str(x) for x in [8]]
-dicSplits = [str(x) for x in [1]]
-tableSplits = [str(x) for x in [1]]
-replicas = [str(x) for x in [1]]
+numTrees = ["10"] #[str(x) for x in [10, 20, 40, 80, 100, 200, 1000]]
+depth = ["4"] #[str(x) for x in [4, 8, 16, 32, 64, 128]]
+mpc = ["15"] #[str(x) for x in [15, 20, 30, 40]]
+maxUnk = ["8"] #[str(x) for x in [8, 10, 12, 14, 16]]
+dicSplits = ["1"] #[str(x) for x in [1, 2, 4, 8, 16, 20, 23, 24]]
+tableSplits = ["1"] #[str(x) for x in [1, 2, 4, 8, 16, 20, 23, 24]]
+replicas = ["1"] #[str(x) for x in [1, 2, 4, 8, 16, 20, 23, 24]]
 coresAvailable = "23" #subtract one from the total system cores to save it to the client
 """
 Dataset to test.
