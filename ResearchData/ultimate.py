9ee3d0e35d2bf1bb3559176d471caab4487d3dab
EduardoRomero83 <earg8395@hotmail.com>
2023-04-08 18:09:15 -0400

    Error message on population exception

--- ResearchData/ultimate.py
+++ ResearchData/ultimate.py
@@ -139,5 +139,5 @@ with open(outfile, "w") as csvfile:
       try:
         populateRow(filesToFind[i], content[delims[i][0]:delims[i][1]], row) 
       except:
-        print(nameData)
+        print("Exception with " + nameData)
     c.writerow(row)
