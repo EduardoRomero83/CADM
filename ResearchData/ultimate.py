33a29f6d08023194186337575386a1b2930adae1
EduardoRomero83 <earg8395@hotmail.com>
2023-05-11 11:48:13 -0400

    Fix combined files

--- ResearchData/ultimate.py
+++ ResearchData/ultimate.py
@@ -8,11 +8,14 @@ indir = "./ResearchData/" + dataset + "merged/"
 outfile = "./ResearchData/" + dataset + "combined.csv"
 out = []
 
-filesToFind = ["acc.py", "time.txt", "acc.s", "pythonperf", "0.serverperf", "1.serverperf"]
+baseFilesTofind = ["acc.py", "acc.s", "pythonperf", "0.serverperf", "1.serverperf"]
 
 def makeHeader():
   header = ["Number of trees", "max depth", "max per cluster", "max unknown"]
-  header.extend(["python exec time", "server exec time", "python acc", "server acc"])
+  header.extend(["replicas", "dic splits", "table splits", "numSamples"])
+  header.extend(["python exec time", "python acc", "server acc"])
+  header.extend(["min core time", "max core time", "average core time in ms"])
+  header.extend(["min diff in ms", "max diff in ms", "cores with no answer"])
   header.extend(["cycles py", "cycles server m0", "cycles server m1"])
   header.extend(["instructions py", "instructions server m0", "instructions server m1"])
   header.extend(["branches py", "branches server m0", "branches server m1"])
@@ -77,6 +80,21 @@ def findServTime(l, row):
   global header
   time = l[findSubstr(l,"Total")].split(':')[1]
   row[findSubstr(header, "server exec")] = float(time)
+  
+def addTimes(l, row, times):
+  global header
+  time = l[findSubstr(l,"Total")].split(':')[1]
+  times.append(float(time) * 1000)
+  minimum = min(times)
+  maximum = max(times)
+  average = sum(times) / len(times)
+  minDiff = average - minimum
+  maxDiff = maximum - average
+  row[findSubstr(header, "min core time")] = minimum
+  row[findSubstr(header, "max core time")] = maximum
+  row[findSubstr(header, "average core time in ms")] = average
+  row[findSubstr(header, "min diff in ms")] = minDiff
+  row[findSubstr(header, "max diff in ms")] = maxDiff
 
 def findServAcc(l, row):
   global header
@@ -107,37 +125,54 @@ def findPerf(l, row, model):
     row[ind1] = int(total)
     row[ind2] = float(percent) 
 
-def populateRow(segment, l, row):
+def populateRow(segment, l, row, timesPerCore):
   global filesToFind
   if len(l) == 0:
     return
   if segment == filesToFind[0]:
     return findAccPy(l, row)
   elif segment == filesToFind[1]:
-    return findServTime(l, row)  
-  elif segment == filesToFind[2]:
     return findServAcc(l, row)
   elif "python" in segment:
     return findPerf(l, row, "py")
+  elif "replica" in segment:
+    return addTimes(l, row, timesPerCore)
   elif "0" in segment:
     return findPerf(l, row, "server m0")
   else:
     return findPerf(l, row, "server m1")
+
+def expandFilesToFind(nameData):
+    global filesToFind
+    numReplicas = int(nameData[5])
+    numDicSplits = int(nameData[6])
+    numTabSplits = int(nameData[7])
+    for i in range(numReplicas):
+        for j in range(numDicSplits):
+            for k in range(numTabSplits):
+                extraFile = "replica" + str(i) + ".dicSplit" + str(j)
+                extraFile = extraFile + ".tabSplit" + str(k) + ".time.txt"
+                filesToFind.append(extraFile)
+    return
 
 with open(outfile, "w") as csvfile:
   c = csv.writer(csvfile)
   header = makeHeader()
   c.writerow(header)
   for f in sorted(os.listdir(indir)):
+    filesToFind = baseFilesTofind[:]
     row = ["_" for i in range(len(header))]
+    timesPerCore = []
     nameData = f.split(".")
-    row[0:4] = [int(x) for x in nameData[1:-1]]
+    row[0:8] = [int(x) for x in nameData[1:-1]]
+    expandFilesToFind(nameData)
     temp = indir + f
     content = open(temp, "r").readlines()
     delims = findLims(content)
     for i in range(len(filesToFind)):
       try:
-        populateRow(filesToFind[i], content[delims[i][0]:delims[i][1]], row) 
+        populateRow(filesToFind[i], content[delims[i][0]:delims[i][1]], row, timesPerCore) 
       except:
         pass
-    c.writerow(row)
+    trimmedRow = [str(x).strip() for x in row]
+    c.writerow(trimmedRow)
