aac127151fbc01a4612fd893c1afc91ee7be69bf
EduardoRomero83 <earg8395@hotmail.com>
2023-05-15 18:39:53 -0400

    Read data as it is sent

--- server/pythonClient/client.py
+++ server/pythonClient/client.py
@@ -57,17 +57,11 @@ print("Sending from client")
 serverOutputFileBase = "server/testaccuracy/temp"
 outputFile = "server/testaccuracy/" + treename + ".compiled.txt"
 
-i = 0
-images = []
-while (i < numSamples):
-    img = infile.read(readSize)
-    images.append(img)
-
 startTime = time.monotonic()
 
 i = 0
 while (i < numSamples):
-    img = images[i]
+    img = infile.read(readSize)
     offset = i % replicas
     for j in range(splits): 
         sArr[offset][j].send(img)
